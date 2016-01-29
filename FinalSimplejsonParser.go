b1f63ae2b294df1baf17cac890ee936ab4029942
Rakesh Raushan <starvader473@gmail.com>
2016-01-29 14:31:16 +0530

    minor changes

--- FinalSimplejsonParser.go
+++ FinalSimplejsonParser.go
@@ -9,13 +9,13 @@ import (	"fmt"
 		)
 
 
-type json struct {
+type Json struct {
 	 Bool bool
 	 String string
 	 Number int
 	 Float float64 
-	 Array []json
-	 Object map[string]json
+	 Array []Json
+	 Object map[string]Json
 	 Parsed bool
 	 Null interface{}
 	 Type string
@@ -23,171 +23,172 @@ type json struct {
 
 
 
-func stringParser(jsonData string) (json, string) {
-	//This function parses string elements and returns the string and remaining jsondata
-	var result json
+func stringParser(JsonData string) (Json, string) {
+	//This function parses string elements and returns the string and remaining Jsondata
+	var result Json
 	var index int                          // stores the index of end of string
-	jsonData = strings.TrimSpace(jsonData) // Trim the data for spaces
-	if jsonData[0] == '"' {  
-		jsonData = jsonData[1:]
-		for jsonData[index] != '"' { //looping over till the end of string
+	JsonData = strings.TrimSpace(JsonData) // Trim the data for spaces
+	if JsonData[0] == '"' {  
+		JsonData = JsonData[1:]
+		for JsonData[index] != '"' { //looping over till the end of string
 			index++
 		}
-		return json{String: jsonData[:index], Parsed: true, Type: "String"}, jsonData[index+1:]
+		return Json{String: JsonData[:index], Parsed: true, Type: "String"}, JsonData[index+1:]
 	}
-	return result, jsonData
+	return result, JsonData
 }
 
 
-func numberParser(jsonData string) (json, string) {
-	//Parses numbers in jsonstring and returns the number and remaining jsonData
-	jsonData = strings.Trim(jsonData, " ")
-	var number json
+func numberParser(JsonData string) (Json, string) {
+	//Parses numbers in Jsonstring and returns the number and remaining JsonData
+	JsonData = strings.Trim(JsonData, " ")
+	var number Json
 	var result [][]int
 	re := regexp.MustCompile("^[-+]?[0-9]*.?[0-9]+([eE][-+]?[0-9]+)?") //init regex
-	result1 := re.FindAllString(jsonData, -1)                          //Library function to find occurrence of string and returns a slice of successive elements
+	result1 := re.FindAllString(JsonData, -1)                          //Library function to find occurrence of string and returns a slice of succe elem
 	if len(result1) > 0 {
-		result = re.FindAllStringIndex(jsonData, -1) //Gives start and end index of the string
+		result = re.FindAllStringIndex(JsonData, -1) //Gives start and end index of the string
 		index := result[0][1] //index is the end  index of the number
 		j,err := strconv.Atoi(result1[0])
 		if err == nil{
-			return json{Number: j, Parsed: true, Type: "Number"}, jsonData[index:]
+			return Json{Number: j, Parsed: true, Type: "Number"}, JsonData[index:]
 		}
 		i,err := strconv.ParseFloat(result1[0],64)
 		if err == nil {
-			return json{Float: i,Parsed: true, Type: "Float"}, jsonData[index:]
+			return Json{Float: i,Parsed: true, Type: "Float"}, JsonData[index:]
 		}
 	} else {
-		return number, jsonData
+		return number, JsonData
 	}
-	return number,jsonData
+	return number,JsonData
 }
 
-func arrayParser(jsonData string) (json,string) {
-	//Parses json arrays and stores them into the user defined type else returns the data
-	var result json
-	parsed := make([]json,0)
-	if jsonData[0] == '[' {
-		jsonData = jsonData[1:]
-		for len(jsonData) > 0 {
-			result, jsonData = elementParser(jsonData)
+func arrayParser(JsonData string) (Json,string) {
+	//Parses Json arrays and stores them into the user defined type else returns the data
+	var result Json
+	parsed := make([]Json,0)
+	if JsonData[0] == '[' {
+		JsonData = JsonData[1:]
+		for len(JsonData) > 0 {
+			result, JsonData = elementParser(JsonData)
 			if result.Parsed == true {
 				parsed = append(parsed,result)
-				jsonData = commaParser(jsonData)
+				JsonData = commaParser(JsonData)
 			}
-			if jsonData[0] == ']'{
+			if JsonData[0] == ']'{
 				fmt.Println("End of array")
-				return json{Array: parsed, Parsed: true, Type: "Array"},jsonData[1:]
+				return Json{Array: parsed, Parsed: true, Type: "Array"},JsonData[1:]
 			}
 		}
 	}
-	return result,jsonData
+	return result,JsonData
 }
 
-func objParser(jsonData string) (json,string) {
-	//Parses json arrays and stores them into the user defined type else returns the data
-	var result json
+func objParser(JsonData string) (Json,string) {
+	//Parses Json arrays and stores them into the user defined type else returns the data
+	var result Json
 	var key string
-	parsed := make(map[string]json)
-	if jsonData[0] == '{' {
-		jsonData = jsonData[1:]
-		for len(jsonData) > 0 {
-			result,jsonData = stringParser(jsonData)
+	parsed := make(map[string]Json)
+	if JsonData[0] == '{' {
+		JsonData = JsonData[1:]
+		for len(JsonData) > 0 {
+			result,JsonData = stringParser(JsonData)
 			if result.Parsed == true {
 				key = result.String
 			}
-			jsonData = colonParser(jsonData)
-			result, jsonData = elementParser(jsonData)
+			JsonData = colonParser(JsonData)
+			result, JsonData = elementParser(JsonData)
 			if result.Parsed == true {
 				parsed[key] = result
-				jsonData = commaParser(jsonData)
+				JsonData = commaParser(JsonData)
 			}
-			if jsonData[0] == '}'{
+			if JsonData[0] == '}'{
 				fmt.Println("End of Object")
-				return json{Object: parsed, Parsed: true, Type: "Object"},jsonData[1:]
+				return Json{Object: parsed, Parsed: true, Type: "Object"},JsonData[1:]
 			}
 		}
 	}
-	return result,jsonData
+	return result,JsonData
 }
 
-func colonParser(jsonData string) string {
-	//Parses colon in json objects and returns the colon and the remaining jsonData
-	jsonData = strings.Trim(jsonData, " ")
-	if jsonData[0] == ':' {
-		return jsonData[1:]
+func colonParser(JsonData string) string {
+	//Parses colon in Json objects and returns the colon and the remaining JsonData
+	JsonData = strings.Trim(JsonData, " ")
+	if JsonData[0] == ':' {
+		return JsonData[1:]
 	}
-	return jsonData
+	return JsonData
 }
 
-func commaParser(jsonData string) string {
-	//Parses comma which saperate elements and returns comma and the remaining jsonData
-	jsonData = strings.Trim(jsonData, " ")
-	if jsonData[0] == ',' {
-		return jsonData[1:]
+func commaParser(JsonData string) string {
+	//Parses comma which saperate elements and returns comma and the remaining JsonData
+	JsonData = strings.Trim(JsonData, " ")
+	if JsonData[0] == ',' {
+		return JsonData[1:]
 	}
-	return jsonData
+	return JsonData
 }
 
-func booleanParser(jsonData string) (json, string) {
-	// Function parses boolean elements and returns the bool value and the remaining jsondata
-	var result json
-	jsonData = strings.Trim(jsonData, " ")
-	if len(jsonData) > 4 {
-		if jsonData[0:4] == "true" {
-			return json{Bool: true, Parsed: true, Type: "Bool"}, jsonData[4:] //slicing the jsonData
-		} else if jsonData[0:5] == "false" {
-			return json{Bool: false, Parsed: true ,Type:"Bool"}, jsonData[5:]
+func booleanParser(JsonData string) (Json, string) {
+	// Function parses boolean elements and returns the bool value and the remaining Jsondata
+	var result Json
+	JsonData = strings.Trim(JsonData, " ")
+	if len(JsonData) > 4 {
+		if JsonData[0:4] == "true" {
+			return Json{Bool: true, Parsed: true, Type: "Bool"}, JsonData[4:] //slicing the JsonData
+		} else if JsonData[0:5] == "false" {
+			return Json{Bool: false, Parsed: true ,Type:"Bool"}, JsonData[5:]
 		}
 	}
-	return result, jsonData
+	return result, JsonData
 }
 
-func nullParser(jsonData string)  (json ,string) {
-	//Parses null values form jsonstring and returns the nil value and remaining jsonData
-	jsonData = strings.Trim(jsonData, " ")
-	var result json
-	if len(jsonData) > 4 {
-		if jsonData[0:4] == "null" {
-			return json{Null:nil, Parsed: true, Type: "Null"}, jsonData[4:]
+func nullParser(JsonData string)  (Json ,string) {
+	//Parses null values form Jsonstring and returns the nil value and remaining JsonData
+	JsonData = strings.Trim(JsonData, " ")
+	var result Json
+	if len(JsonData) > 4 {
+		if JsonData[0:4] == "null" {
+			return Json{Null:nil, Parsed: true, Type: "Null"}, JsonData[4:]
 		}
 	}
-	return result, jsonData
+	return result, JsonData
 }
 
 
-func elementParser(jsonData string) (json, string) {
-	//Function tries all the parsers one by one on each element and returns the result and the remaining jsonData
-	var result json
-	result, jsonData = stringParser(jsonData)
+func elementParser(JsonData string) (Json, string) {
+	//Function tries all the parsers one by one on each element and returns the result and the remaining JsonData
+	var result Json
+	result, JsonData = stringParser(JsonData)
 	if result.Parsed == true {
-		return result, jsonData
+		return result, JsonData
 	}
-	result, jsonData = numberParser(jsonData)
+	result, JsonData = numberParser(JsonData)
 	if result.Parsed == true {
-		return result, jsonData
+		return result, JsonData
 	}
 	
-	result, jsonData = booleanParser(jsonData)
+	result, JsonData = booleanParser(JsonData)
 	if result.Parsed == true {
-		return result, jsonData
+		return result, JsonData
 	}
-	result, jsonData = nullParser(jsonData)
+	result, JsonData = nullParser(JsonData)
 	if result.Parsed == true {
-		return result, jsonData
+		return result, JsonData
 	}
-	result, jsonData = arrayParser(jsonData)
+	result, JsonData = arrayParser(JsonData)
 	if result.Parsed == true {
-		return result,jsonData
+		return result,JsonData
 	}
-	result, jsonData = objParser(jsonData)
+	result, JsonData = objParser(JsonData)
 	if result.Parsed == true {
-		return result,jsonData
+		return result,JsonData
 	}
-	return result, jsonData
+	return result, JsonData
 }
 
-func (m json) getElement() interface{} {
+//Utility function to acess the parsed json
+func (m Json) getElement() interface{} {
 	Type := m.Type
 	switch Type {
 		case "String":
@@ -227,8 +228,6 @@ func main(){
 		result,_ :=objParser(data)
 		fmt.Println(result)
 		k := result.getElement()
-		fmt.Println(k)
-		
+		fmt.Println(k)		
 	}
-	os.Exit(0)
 }
